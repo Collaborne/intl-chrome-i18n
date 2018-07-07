fb64c7ebd1c005694747c2ac68a882208f171615
Ronny Roeller <ronny@collaborne.com>
2018-07-07 15:16:07 +0200

    Simplify TypeScript definition

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,8 @@
-declare class ChromeI18n {
-	constructor(locale: string, defaultLocale: string, pathToLocales: string);
-	getMessage(messageName: string, content: object, formats: object): string;
-}
 declare module 'intl-chrome-i18n' {
+	class ChromeI18n {
+		constructor(locale: string, defaultLocale: string, pathToLocales: string);
+		getMessage(messageName: string, content: object, formats?: object): string;
+	}
 	export = ChromeI18n;
 }
+
