799279b671b642948f1340511aee74d39755328d
Ronny Roeller <ronny@collaborne.com>
2018-07-13 16:38:20 +0200

    Content is optional (for translations without placeholders)

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare module 'intl-chrome-i18n' {
 	class ChromeI18n {
 		constructor(locale: string, defaultLocale: string, pathToLocales: string);
-		getMessage(messageName: string, content: object, formats?: object): string;
+		getMessage(messageName: string, content?: object, formats?: object): string;
 	}
 	export = ChromeI18n;
 }
